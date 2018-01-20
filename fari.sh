01cd6a8eec422b2a88727bd9cdd40b7356b682d1
Damien Pollet <damien.pollet@gmail.com>
2018-01-20 21:50:09 +0100

    Update docs

--- fari.sh
+++ fari.sh
@@ -14,19 +14,18 @@
 # $ fari.sh
 # ```
 #
-# **Install:** Drop or link
+#
+# ## Install
+#
+# Drop or link
 # [`fari.sh`](https://raw.githubusercontent.com/cdlm/fari.sh/master/fari.sh) in
 # your `$PATH`.
 #
-# The [Fari source][github] is available on Github, and is released under the
-# [MIT license][mit]. See the [Docco][] generated docs for more information:
-# https://cdlm.github.io/fari.sh/fari.html
 #
-# * * *
+# ## Configuration
 #
-# **Configuration:** To have code automatically loaded in the fresh image, add a
-# `load.st` file containing the needed code snippet in your project, typically
-# something like:
+# To have code automatically loaded in the fresh image, add a `load.st` file
+# containing the needed code snippet in your project, typically something like:
 #
 # ```smalltalk
 # "load.st"
@@ -35,7 +34,7 @@
 #   load.
 # ```
 #
-# This will generate a `pharo-$githash.image` file. The git hash comes from the
+# This will generate a `pharo-1c0ffee.image` file. The hex suffix comes from the
 # downloaded snapthot and identifies which sources file matches the image.
 #
 # **Named images:** Instead of `load.st`, you can also use a named load script,
@@ -48,15 +47,37 @@
 # loaded after the load scripts; those are intended for loading personal tools
 # and settings, and should thus be left out of version control.
 #
-# In the absence of a named load script, the name prefix defaults to `pharo`; to
-# change it, set the `PHARO_PROJECT` environment variable; we recommend
-# [direnv][] to make that setting persistent and project-specific.
+# **Environment variables:** Fari takes a few environment variables into
+# account. We recommend [direnv][] to make any setting persistent and
+# project-specific.
+#
+# `PHARO_PROJECT`: image name used in the absence of a named load script;
+# defaults to `pharo`.
+#
+# `PHARO`: name of the Pharo VM command-line executable. Defaults to `pharo-ui`,
+# assuming that you have it in your `$PATH`. If you get your VMs from
+# [get.pharo.org][], set it to `./pharo-ui`.
+#
+# `PHARO_VERSION`: Pharo release, as used in the [get.pharo.org][] URLs;
+# defaults to `70`.
+#
+# `PHARO_FILES`: URL prefix for downloading the image; defaults to
+# `http://files.pharo.org/get-files/${PHARO_VERSION}`.
+#
+# ## License
+#
+# The [Fari source][github] is available on Github, and is released under the
+# [MIT license][mit]. See the [Docco][] generated docs for more information:
+# https://cdlm.github.io/fari.sh
 #
 # [github]: https://github.com/cdlm/fari.sh
 # [mit]: http://opensource.org/licenses/MIT
 # [pharo]: http://pharo.org
-# [docco]: http://ashkenas.com/docco/
+# [get.pharo.org]: http://get.pharo.org
+# [docco]: http://ashkenas.com/docco
 # [direnv]: https://direnv.net
+
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 # Now let's start. First & foremost, we toggle [bash strict
 # mode](http://redsymbol.net/articles/unofficial-bash-strict-mode/).
